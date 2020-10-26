a72bcda707eff74f9bc97bc7603d6e1b9b7eab0d
pdjdev <pdj5096@gmail.com>
2020-10-26 22:08:29 +0900

    초기 커밋.

---
+++ bokboot/Form1.Designer.vb
@@ -0,0 +1,40 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(10, 10)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+End Class
---
+++ bokboot/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Collections.Specialized
+
+Public Class Form1
+    Private Const WM_DRAWCLIPBOARD As Integer = &H308
+    Private Declare Function SetClipboardViewer Lib "user32" Alias "SetClipboardViewer" (ByVal hwnd As Integer) As Integer
+    Dim result As Integer = SetClipboardViewer(MyBase.Handle.ToInt32)
+
+    Public prevClipboardData As IDataObject = Nothing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_DRAWCLIPBOARD Then
+            Beep()
+            ClipboardScanner()
+
+            '
+            '이벤트
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Public Sub ClipboardScanner()
+        If Clipboard.ContainsText() Then
+            Dim d = Clipboard.GetText()
+            MsgBox(d)
+
+        ElseIf Clipboard.ContainsFileDropList Then
+
+            Dim fileCollection As StringCollection = Clipboard.GetFileDropList()
+
+            For Each f In fileCollection
+                MsgBox(f)
+            Next
+        End If
+    End Sub
+End Class
